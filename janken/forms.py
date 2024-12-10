7d2243ea0ef4147eebc51c08ee2db399f3101e3e
itsuneki <itsuneki@skywill.jp>
2024-12-10 12:09:39 +0900

    フォームの作成

---
+++ janken/forms.py
@@ -0,0 +1,18 @@
+"""
+フォーム
+・ユーザー登録用
+・ログイン用
+"""
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class SignupUserForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'image']
+
+class LoginForm(AuthenticationForm):
+    pass
